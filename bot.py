a5b80240d946c068d256139e2fb8f80a2373ccf3
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-09-16 07:50:02 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,10 @@
-from pyrogram import Client, filters
-import asyncio
-app = Client(
-  "FuckYjeueuou",
-  api_id = 9398500,
-  api_hash = "ad2977d673006bed6e5007d953301e13",
-  bot_token = "5776886346:AAHA-WOxRU_ODu5EnGl22oouqICHQBfmoD0"
-)
-@app.on_message(filters.media & filters.forwarded)
-@app.on_message(filters.media & filters.private)
-async def test(client, message):
-  try:
-    location = "./github/workflows/"
-    x = await app.download_media(message,file_name="./github/workflows/app.ipa")
-    print(x)
-    await app.send_document(message.chat.id,file_name="./github/workflows/app.ipa")
-    await message.reply("done")
-  except Exception as e:
-    print(e)
+import flask
+from flask import Flask
 
+app = Flask(__name__)
 
-app.run()
+@app.route("/")
+def home():
+  return "hi"
+if __name__ == "__main__":
+  app.run(host="0.0.0.0",port=8080)
