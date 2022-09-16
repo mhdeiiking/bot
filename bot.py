b4ed4e6cef2f06629a01d56adb741389d5dffa63
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-09-16 13:45:34 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
-import flask
-from flask import Flask
+import requests,re,telebot
+import time
+from time import sleep 
+bot = telebot.TeleBot("5776886346:AAETy6yybVxkPnKCtxWHZVECHiSPDG7OwBIc")
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-  return "hi"
-if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=8080)
+r = (requests.get("https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/").text)
+@bot.channel_post_handler(commands=['start'])
+def start(message):
+    for i in range(0,114):
+        a = (re.findall('<a href="(.*?).mp3">(.*?).mp3</a>',r)[i][i])
+        print(a)
+        bot.send_audio(message.chat.id,audio=f"https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/001.mp3",caption="استغفرالله .")
+        sleep(6)
+    bot.send_message(message.chat.id,f"انتهى النشر .")
+bot.infinity_polling()
