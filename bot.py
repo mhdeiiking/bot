6178138dca3e92a403b15d5e33285b9598543a4a
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-09-16 07:09:22 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,19 @@
-
-import asyncio
-import os
-import re
-
 from pyrogram import Client, filters
-from pyrogram.types import (
-    InputMediaPhoto,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
-    Message,
+app = Client(
+  "FuckYjeueuou",
+  api_id = 9398500,
+  api_hash = "ad2977d673006bed6e5007d953301e13",
+  bot_token = "5776886346:AAHA-WOxRU_ODu5EnGl22oouqICHQBfmoD0"
 )
+@app.on_message(filters.media & filters.private)
+def test(client, message):
+  try:
+    location = "./"
+    x = message.download(location)
+    print(x)
+    message.reply("done")
+  except Exception as e:
+    print(e)
 
-bot = Client(
-    "Bot",
-    api_id = 14706934,
- api_hash = "fc81a74886bae0a8499886ff4561be65",
- bot_token = "5448584711:AAEDCP1Glg7fjIhBWnaROKsKBWQHY-nJWGw"
-)
 
-
-@bot.on_message(filters.command("post") & filters.group)
-async def start(client, message):
-    from pyrogram.enums import ChatMemberStatus
-    state = (await client.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)).status
-    if state == ChatMemberStatus.OWNER:
-        bots = []
-        async for m in bot.get_chat_members(message.chat.id):
-            bots.append(m.user.id)
-        for b in bots:
-            import random,time
-            username = (await client.get_chat(b)).username
-            l = ["فايت للكروب وماتتفاعل ؟ ","تعال اعزمك  ع موطه ","تحبني لولا؟","ممكن نتعرف ؟","ناس تريدك تعالل !","شلونك؟","هلو","ها","تعال محتاجك " ,"مشتاقلك ","شلونك","ممكن بوسه","هلو"]
-            r = random.choice(l)
-            await bot.send_message(message.chat.id,f"{r} @{username}")
-            await asyncio.sleep(100)
-        start(client,message)
-
-bot.run()
+app.run()
