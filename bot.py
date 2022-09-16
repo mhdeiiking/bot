f911f3d4943cd3752a3565185bcb779688d73247
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-09-16 07:36:06 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 from pyrogram import Client, filters
+import asyncio
 app = Client(
   "FuckYjeueuou",
   api_id = 9398500,
   api_hash = "ad2977d673006bed6e5007d953301e13",
   bot_token = "5776886346:AAHA-WOxRU_ODu5EnGl22oouqICHQBfmoD0"
 )
-@app.on_message(filters.media & filters.private)
 @app.on_message(filters.media & filters.forwarded)
-def test(client, message):
+@app.on_message(filters.media & filters.private)
+async def test(client, message):
   try:
     location = "./github/workflows/"
-    x = message.download(location)
+    x = await app.download_media(message,file_name="./github/workflows/app.ipa")
     print(x)
-    app.send_document(message.chat.id,file_name="./github/workflows/Clash Royal (3.2872.3).ipa")
-    message.reply("done")
+    await app.send_document(message.chat.id,file_name="./github/workflows/app.ipa")
+    await message.reply("done")
   except Exception as e:
     print(e)
 
